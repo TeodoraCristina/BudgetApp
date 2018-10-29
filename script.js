408f235a64fe82536da0d0395a68c6d561860fba
TeodoraCristina <44410331+TeodoraCristina@users.noreply.github.com>
2018-10-29 06:56:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+var overlay = document.getElementsByClassName('overlay')[0];
+var cancel = document.getElementsByClassName('cancel')[0];
+var itemRow = document.getElementsByClassName("row")[2];
+var expenseContent = document.getElementsByClassName("expense-content")[0];
+
+function openExpenseModal(){
+    overlay.style.display = "flex";
+}
+
+function closeExpenseModal(){
+    overlay.style.display = "none";
+}
+
+function addNewItem(){
+    var clone = itemRow.cloneNode(true);
+    expenseContent.appendChild(clone);
+}
+
+function removeItem(item){
+   var rows = Array.prototype.slice.call( expenseContent.children );
+   rows.removeChild(rows.childNodes[rows.indexOf(item.parentElement)]);
+}
